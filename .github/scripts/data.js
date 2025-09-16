ef6ad45219dbcfee3512e1afe0cb611f4440702e
Maximus7474 <the.maximus.7474@gmail.com>
2025-09-16 11:31:16 +0200

    feat(scripts): base functions & data

---
+++ .github/scripts/data.js
@@ -0,0 +1,20 @@
+const defaultLocaleFile = 'en.json';
+
+const resources = [
+  { name: 'resource_1',   link: 'https://creations.mtdv.me/watch?v=Z5OI9Df2fY' },
+  { name: 'resource_2',   link: 'https://creations.mtdv.me/watch?v=Z5OI9Df2fY' },
+];
+
+const localeLabels = {
+  'en': 'English (en)',
+  'de': 'German (de)',
+  'fr': 'French (fr)',
+  'es': 'Spanish (es)',
+  'it': 'Italian (it)',
+};
+
+module.exports = {
+  defaultLocaleFile,
+  resources,
+  localeLabels
+}
